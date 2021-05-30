696437e00d547218751bdc676418807994e2e8cd
kavindu <kavindu@wso2.com>
2021-05-30 15:42:57 +0530

    Save username and password in EEPROM

--- README.md
+++ README.md
@@ -4,3 +4,15 @@
         http://192.168.20.1/?ssid=SLT&pwd=bmVWZXJnMXYjdVA=
 
 2. url to stop  AP in ESP and connect to user's WiFi -
+
+## EEPROM data format
+
+location 1 - The given constant value (`EEPROM_CONST`)
+
+location 2 - Length of SSID(`len_SSID`)
+
+From location 3 to location 3 + len_SSID - Characters of SSID
+
+location 3 + len_SSID + 1 - Length of password(`len_PWD`)
+
+From location 3 + len_SSID + 2 to location 3 + len_SSID + 2 + len_PWD - Characters on Password
--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,19 @@
 #include <WiFiUdp.h>
 #include <base64.h>
 #include <stdint.h>
+#include <EEPROM.h>
+
+#define LEFT 4
+#define RIGHT 5
+#define EEPROM_SIZE 100
 
 const char *ssid = "ESP";
 const char *password = "12345678";
 
-const char *userSSID;
-char userPassWord[100];
+char userSSID[100]; // TODO : Change this based on the requirement
+char userPassWord[100]; // TODO : Change this based on the requirement
+uint pwdDecodeLen = 0;
+uint ssidLen = 0;
 
 IPAddress staticIp(192, 168, 1, 20);
 
@@ -29,16 +36,17 @@ double voltage = 2.5;
 int dB = 34;
 unsigned long t = millis();
 bool isUrgent = false;
-int i = 0; // for testing
 uint8_t intArr[] = {4, 3, 2, 1};
 
-void connectUserWiFi() {
-    // WiFi.disconnect();
+bool connectUserWiFi() {
     WiFi.softAPdisconnect();
     server.close();
 
-    WiFi.mode(WIFI_STA);
+    if (!WiFi.mode(WIFI_STA)) {
+        Serial.println("WIFI_STA Failed");
+    }
     if (WiFi.config(staticIp, IPAddress(192, 168, 1, 1), IPAddress(255, 255, 0, 0))) {
+        // This config fails. Should check it
         Serial.println("STA Failed to configure");
     }
 
@@ -46,9 +54,14 @@ void connectUserWiFi() {
 
     Serial.print("Connecting to ");
     Serial.print(userSSID);
+    int i = 100; // Wait for 10 seconds
     while (WiFi.status() != WL_CONNECTED) {
         delay(100);
         Serial.print(".");
+        i--;
+        if (i < 0) {
+            return false;
+        }
     }
 
     Serial.println();
@@ -57,17 +70,56 @@ void connectUserWiFi() {
 
     Udp.begin(localUdpPort);
     Serial.print("Started UDP communication ");
+    return true;
+}
+
+void saveToROM() {
+    int totLen = ssidLen + pwdDecodeLen + 3;
+    int nextPos = 0;
+    if (totLen > EEPROM_SIZE) {
+        EEPROM.begin(totLen);
+    } else {
+        EEPROM.begin(EEPROM_SIZE);
+    }
+
+    EEPROM.write(nextPos, totLen);
+    EEPROM.commit();
+    nextPos = nextPos + 1;
+    EEPROM.write(nextPos, ssidLen);
+    EEPROM.commit();
+    nextPos = nextPos + 1;
+    for (size_t i = 0; i < ssidLen; i++) {
+        EEPROM.write(nextPos + i, userSSID[i]);
+        EEPROM.commit();
+    }
+    nextPos = nextPos + ssidLen;
+    EEPROM.write(nextPos, pwdDecodeLen);
+    EEPROM.commit();
+    nextPos = nextPos + 1;
+    for (size_t i = 0; i < pwdDecodeLen; i++) {
+        EEPROM.write(nextPos + i, userPassWord[i]);
+        EEPROM.commit();
+    }    
 }
 
 void getUserWiFiInfo() {
     userssid = server.arg(0);
-    userSSID = userssid.c_str();
+    ssidLen = userssid.length();
+    Serial.printf("SSID Len : %d\n", ssidLen);
+    userssid.toCharArray(userSSID, ssidLen + 1);
+    for (size_t i = 0; i < ssidLen; i++) {
+        Serial.printf("%c", userSSID[i]);
+    }
+    
+    Serial.printf("SSID : %s\n", userSSID);
 
     userPwd = server.arg(1);
     int pwdLen = userPwd.length();
     char s[pwdLen + 1];
     userPwd.toCharArray(s, pwdLen);
-    b64_decode(userPassWord, s, pwdLen - 1);
+    pwdDecodeLen = b64_decode(userPassWord, s, pwdLen - 1);
+
+    saveToROM();
 
     server.send(200, "text/plain", "hello from esp8266!");
     Serial.println("Waiting for 3 seconds");
@@ -75,15 +127,69 @@ void getUserWiFiInfo() {
     connectUserWiFi();
 }
 
-void setup(void) {
-    Serial.begin(115200);
-    Serial.println("");
+void readDataFromROM() {
+    int totLen = ssidLen + pwdDecodeLen + 3;
+    if (totLen > EEPROM_SIZE) {
+        EEPROM.begin(totLen);
+    } else {
+        EEPROM.begin(EEPROM_SIZE);
+    }
+
+    int nextPos = 1;
+    ssidLen = EEPROM.read(nextPos);
+    Serial.printf("%d\n", ssidLen);
+    nextPos = nextPos + 1;
+    for (size_t i = 0; i < ssidLen; i++) {
+        userSSID[i] = EEPROM.read(nextPos + i);
+        Serial.printf("%c", userSSID[i]);
+    }
+    nextPos = nextPos + ssidLen;
+    pwdDecodeLen = EEPROM.read(nextPos);
+    Serial.printf("\n%d\n", pwdDecodeLen);
+    nextPos = nextPos + 1;
+    for (size_t i = 0; i < pwdDecodeLen; i++) {
+        userPassWord[i] = EEPROM.read(nextPos + i);
+        Serial.printf("%c", userPassWord[i]);
+    }
+}
+
+// For testing
+void writeToROMForTesting() {
+    Serial.println("New entry!");
+    userSSID[0] = 'S';
+    userSSID[1] = 'L';
+    userSSID[2] = 'T';
+
+    userPassWord[0] = 'b'; //bmVWZXJnMXYjdVA=
+    userPassWord[1] = 'm';
+    userPassWord[2] = 'V';
+    userPassWord[3] = 'W';
+    userPassWord[4] = 'Z';
+    userPassWord[5] = 'X';
+    userPassWord[6] = 'J';
+    userPassWord[7] = 'n';
+    userPassWord[8] = 'M';
+    userPassWord[9] = 'X';
+    userPassWord[10] = 'Y';
+    userPassWord[11] = 'j';
+    userPassWord[12] = 'd';
+    userPassWord[13] = 'V';
+    userPassWord[14] = 'A';
+    userPassWord[15] = '=';
+
+    ssidLen = strlen(userSSID);
+    pwdDecodeLen = strlen(userPassWord);
+    saveToROM();
+    readDataFromROM();
+}
+
+void startServer() {
     WiFi.mode(WIFI_AP);
     WiFi.softAPConfig(staticIp, IPAddress(192, 168, 1, 1), IPAddress(255, 255, 0, 0));
     WiFi.softAP(ssid, password);
 
     IPAddress myIP = WiFi.softAPIP();
-    Serial.print("HotSpt IP:");
+    Serial.print("\nHotSpt IP:");
     Serial.println(myIP);
 
     server.on("/", getUserWiFiInfo);
@@ -92,8 +198,13 @@ void setup(void) {
     Serial.println("HTTP server started");
 }
 
+void setup(void) {
+    Serial.begin(115200);
+    readDataFromROM();
+    connectUserWiFi();
+}
+
 void send() {
-    i = i + 1;
     // send back a reply, to the IP address and port we got the packet from
     printf("Ip : %s\n", phoneIp.toString().c_str());
     printf("port : %d", phonePort);
@@ -135,4 +246,9 @@ void loop(void) {
         t = millis();
         send();
     }
+    // for (size_t i = 0; i < 256; i++) {
+    //     analogWrite(LEFT, i);
+    //     analogWrite(RIGHT, 256 - i);
+    //     delay(2500);
+    // }
 }
